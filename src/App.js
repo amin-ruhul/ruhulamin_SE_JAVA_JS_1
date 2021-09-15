50e08db271775a2b9812bc37d9c9d3871a504a45
amin_ruhul <ruhulinfo50@gmail.com>
2021-09-16 00:32:40 +0600

    add login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import NavBar from "./components/NavBar";
 import Slider from "./components/Slider";
 import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
 function App() {
   return (
     <>
       <NavBar />
-      <Register />
+      <Login />
     </>
   );
 }
